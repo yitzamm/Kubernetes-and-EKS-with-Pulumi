06237119986529c20a7edad2bfa36ff3771176de
Scott Lowe <slowe@pulumi.com>
2023-03-17 11:48:48 -0600

    Update for project restructuring
    Change code to only create Kubernetes objects. Add StackReference for information on underlying Kubernetes cluster. Update documentation.
    
    Signed-off-by: Scott Lowe <slowe@pulumi.com>

--- infra/README.md
+++ infra/README.md
@@ -1,9 +1,12 @@
 # Deploying with Pulumi
 
+This project has a dependency on the Kubernetes cluster(s) managed by the Pulumi code in [the `zephyr-k8s` repository](https://github.com/pulumi/zephyr-k8s). You will need to have created a stack from that project and run a successful `pulumi up` before starting here. You will also need to know the organization name, project name, and stack name for the stack that manages the Kubernetes cluster(s). All of this information can be obtained by running `pulumi stack ls` in the directory where the `zephyr-k8s` project resides.
+
 To deploy this application and its associated infrastructure with Pulumi, you should:
 
 * have the Pulumi CLI installed; 
-* have NodeJS installed; and
+* have NodeJS installed;
+* have the `kubectl` CLI tool installed; and
 * have the AWS CLI installed and configured for your AWS account.
 
 Then follow these steps:
@@ -13,11 +16,14 @@ Then follow these steps:
 3. Run `npm install` to install all necessary dependencies.
 4. Run `pulumi stack init <name>` to create a new stack.
 5. Set your desired AWS region with `pulumi config set aws:region <region-name>`.
-6. Run `pulumi up`.
+6. Run `pulumi config set eksOrgName <org-name>` to supply the name of the Pulumi Service organization where the Kubernetes cluster stack was created.
+7. Run `pulumi config set eksProjName <project-name>` to supply the name of the Pulumi Service project where the Kubernetes cluster stack was created.
+8. Run `pulumi config set eksStackName <stack-name>` to supply the name of the Kubernetes cluster stack.
+9. Run `pulumi up`.
 
 **NOTE:** You'll see `Pulumi.test.yaml` and `Pulumi.prod.yaml` stack files in this repository. These are here for illustrative purposes (to tie back to the Pulumi blog series) and will not impact your ability to use the steps above _unless_ you use a stack name of "test" or "prod" for your stack.
 
-After the stack is finished deploying, use `pulumi stack output` to retrieve the Kubeconfig for the newly-created EKS cluster:
+After the stack is finished deploying, use `pulumi stack output` to retrieve the Kubeconfig for the Kubernetes cluster (this is also available as a stack output on the Kubernetes stack):
 
 ```shell
 pulumi stack output kubeconfig > kubeconfig
--- infra/index.ts
+++ infra/index.ts
@@ -1,40 +1,19 @@
 import * as pulumi from "@pulumi/pulumi";
-import * as awsx from "@pulumi/awsx";
-import * as eks from "@pulumi/eks";
 import * as k8s from "@pulumi/kubernetes";
+import * as awsx from "@pulumi/awsx";
 
-// Grab some values from the Pulumi configuration (or use default values)
+// Grab some configuration values
 const config = new pulumi.Config();
-const minClusterSize = config.getNumber("minClusterSize") || 3;
-const maxClusterSize = config.getNumber("maxClusterSize") || 6;
-const desiredClusterSize = config.getNumber("desiredClusterSize") || 3;
-const eksNodeInstanceType = config.get("eksNodeInstanceType") || "t3.medium";
-const vpcNetworkCidr = config.get("vpcNetworkCidr") || "10.0.0.0/16";
+const eksOrgName = config.get("eksOrgName");
+const eksProjName = config.get("eksProjName");
+const eksStackName = config.get("eksStackName");
 
-// Create a new VPC
-const eksVpc = new awsx.ec2.Vpc("eks-vpc", {
-    enableDnsHostnames: true,
-    cidrBlock: vpcNetworkCidr,
-});
-
-// Create the EKS cluster
-const eksCluster = new eks.Cluster("eks-cluster", {
-    vpcId: eksVpc.vpcId,
-    publicSubnetIds: eksVpc.publicSubnetIds,
-    privateSubnetIds: eksVpc.privateSubnetIds,
-    // Change configuration values to change any of the following settings
-    instanceType: eksNodeInstanceType,
-    desiredCapacity: desiredClusterSize,
-    minSize: minClusterSize,
-    maxSize: maxClusterSize,
-    nodeAssociatePublicIpAddress: false,
-    // Uncomment the next two lines for a private cluster (VPN access required)
-    // endpointPrivateAccess: true,
-    // endpointPublicAccess: false
-});
+// Create a StackReference to get Kubeconfig from base stack
+const kubeSr = new pulumi.StackReference(`${eksOrgName}/${eksProjName}/${eksStackName}`);
+const baseKubeconfig = kubeSr.getOutput("kubeconfig");
 
 // Create a new Kubernetes provider for the EKS cluster
-const eksProvider = new k8s.Provider("eks-provider", {kubeconfig: eksCluster.kubeconfig});
+const eksProvider = new k8s.Provider("eks-provider", {kubeconfig: baseKubeconfig});
 
 // Define consistent labels for each component
 const assetsLabels = {
@@ -102,7 +81,6 @@ const uiLabels = {
     "app.kubernetes.io/component": "ui",
     "app.kubernetes.io/managed-by": "pulumi"
 }
-
 
 // Create namespaces for the application components
 const assetsNs = new k8s.core.v1.Namespace("assets-ns", {
@@ -1312,11 +1290,15 @@ const rabbitmqDeployment = new k8s.apps.v1.Deployment("rabbitmq-deployment", {
 
 const uiImage = new awsx.ecr.Image("ui-image", {
     repositoryUrl: repository.url,
-    dockerfile: "../images/java17/Dockerfile",
-    path: "../src/ui",
+    dockerfile: "/Users/slowe/Work/Code/Repos/zephyr-app/images/java17/Dockerfile",
+    path: "/Users/slowe/Work/Code/Repos/zephyr-app/src/ui",
     args: {
         JAR_PATH: "target/ui-0.0.1-SNAPSHOT.jar",
     },
+    env: {
+        DOCKER_BUILDKIT: "1",
+        DOCKER_DEFAULT_PLATFORM: "linux/amd64"
+    }
 });
 
 const uiDeployment = new k8s.apps.v1.Deployment("ui-deployment", {
@@ -1405,5 +1387,4 @@ const uiDeployment = new k8s.apps.v1.Deployment("ui-deployment", {
 }, { provider: eksProvider });
 
 // Export some values for use elsewhere
-export const kubeconfig = eksCluster.kubeconfig;
-export const vpcId = eksVpc.vpcId;
+export const kubeconfig = baseKubeconfig;
